0bdfdbaa8d9e3d6ce9be51c46249348bddcb3259
anto-christo <anto.christo.20@gmail.com>
2018-10-24 12:15:45 +0530

    Add common todomvc files

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-require('./js');
+require('./js/app.js');
 require('bootstrap/dist/css/bootstrap.css');
--- src/js/misc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    resetFields: function() {
-        localStorage.event = null;
-        $('#date').val("");
-        $('#venue').val("");
-        $('#title').val("");
-        $('#description').val("");
-    }
-}
