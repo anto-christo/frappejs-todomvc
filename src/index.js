f8b862d5f3ea0417f75fe9f075bf4ceba06789fa
anto-christo <anto.christo.20@gmail.com>
2018-10-25 15:45:24 +0530

    Make a separate todo component and remove router

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ require('todomvc-common/base.css');
 require('todomvc-app-css/index.css');
 require('todomvc-common/base.js');
 require('./js/store.js');
-require('./js/routes.js');
 
 import Vue from 'vue/dist/vue.js';
 import App from './App.vue';
 
 new Vue({
-	el: '#todoapp',
+	el: '#todo',
 	template: '<App/>',
 	components: {App},
 });
