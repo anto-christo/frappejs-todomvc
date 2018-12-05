77f55a6f5ccf207eb4827a62edc58e0874a59352
anto-christo <anto.christo.20@gmail.com>
2018-12-05 16:08:43 +0530

    Add electrono specific files

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,28 @@
 import frappe from 'frappejs';
 import common from 'frappejs/common';
 import models from '../models';
-import HTTPClient from 'frappejs/backends/http';
+import SQLite from 'frappejs/backends/sqlite';
 import Observable from 'frappejs/utils/observable';
 import Todo from './components/Todo.vue'
+import path from 'path';
 
-const server = 'localhost:8000';
 window.frappe = frappe;
 frappe.init();
 frappe.registerLibs(common);
 frappe.registerModels(models);
 frappe.fetch = window.fetch.bind();
-frappe.db = new HTTPClient({ server });
 frappe.docs = new Observable();
+
+async function connectToDatabase() {
+    const dbPath = path.resolve(process.cwd(),'test.db');
+    frappe.isServer = true;
+    frappe.login('Administrator');
+    frappe.db = new SQLite({ dbPath: dbPath });
+    await frappe.db.connect();
+    await frappe.db.migrate();
+}
+
+connectToDatabase();
 
 export default {
     components: {
